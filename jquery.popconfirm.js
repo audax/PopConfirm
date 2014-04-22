7ad6c85381e5b99d6f8e180bf39d606ec60bc40f
Jens Kadenbach <gitdax@daxbau.net>
2014-04-22 11:48:41 +0200

    Removed implicit callbacks and introduced yesCallback and noCallback

--- jquery.popconfirm.js
+++ jquery.popconfirm.js
@@ -20,7 +20,9 @@
 				placement: 'right',
 				container: 'body',
 				yesBtn:   'Yes',
-				noBtn:    'No'
+				noBtn:    'No',
+                yesCallBack: null,
+                noCallBack: null,
 			};
 			var options =  $.extend(defaults, options);
 			var last = null;
@@ -29,45 +31,7 @@
 				var self = $(this);
 				var arrayActions = [];
 				
-				// If there are jquery click events
-				if (typeof(jQuery._data(this, "events")) != "undefined" && typeof(jQuery._data(this, "events")['click']) != "undefined") {
-					
-					// Save all click handlers
-					for (var i = 0; i < jQuery._data(this, "events")['click'].length; i++) {
-						arrayActions.push(jQuery._data(this, "events")['click'][i].handler);
-					}
-					
-					// unbind it to prevent it firing
-					$(self).unbind("click");
-				}
-				
-				// If there are hard onclick attribute
-				if(typeof self.attr('onclick') != 'undefined') {
-					// Extracting the onclick code to evaluate and bring it into a closure
-					var code = self.attr('onclick');
-					arrayActions.push(function() {
-						eval(code);
-					});
-					$(self).removeAttr('onclick');
-				}
-				
-				// If there are href link defined
-				if(typeof self.attr('href') != 'undefined') {
-					// Assume there is a href attribute to redirect to
-					arrayActions.push(function() {
-						window.location.href = self.attr('href');
-					});
-				}
-				
-				// If the button is a submit one
-				if(typeof self.attr('type') != 'undefined' && self.attr('type') == 'submit') {
-					// Get the form related to this button then store submiting in closure
-					var form = $(this).parents('form:first');
-					arrayActions.push(function() {
-						form.submit();
-					});
-				}
-				
+
 				self.popover({
 					trigger: 'manual',
 					title: options.title,
@@ -77,7 +41,7 @@
 					content: options.content + '\
 						<p class="button-group" style="margin-top: 10px; text-align: center;">\
 							<button type="button" class="btn btn-small btn-danger confirm-dialog-btn-confirm">' + options.yesBtn + '</button>\
-							<button type="button" class="btn btn-small confirm-dialog-btn-abord">' + options.noBtn + '</button>\
+							<button type="button" class="btn btn-small confirm-dialog-btn-abort">' + options.noBtn + '</button>\
 						</p>'
 				}).click(function(e) {
 					if(last && last !== self) last.popover('hide');
@@ -94,14 +58,16 @@
 					
 					self.popover('show');
 					
-					self.next('.popover').find('.confirm-dialog-btn-confirm').bind('click', function(e) {
-						for(var i = 0; i < arrayActions.length; i++) {
-							arrayActions[i].apply(self);
-						}
-						
+					$('.popover').find('.confirm-dialog-btn-confirm').bind('click', function(e) {
+                        if (options.yesCallBack) {
+                            options.yesCallBack(self);
+                        }
 						self.popover('hide');
 					});
-					self.next('.popover').find('.confirm-dialog-btn-abord').bind('click', function(e) {
+					$('.popover').find('.confirm-dialog-btn-abort').bind('click', function(e) {
+                        if (options.noCallBack) {
+                            options.noCallBack(self);
+                        }
 						self.popover('hide');
 					});
 				});
